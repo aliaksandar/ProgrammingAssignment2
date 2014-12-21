3b280a9c33b7e21a7a27db6c8c88cf93d70efb71
Aliaksandr Novik <aliaksandr_novik@epam.com>
2014-12-22 01:21:33 +0300

    implement functions that decrease computation of matrices inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions should be used to decrease the cost of computation
+## of matrices inversion. 
+## The main idea is to cache previously calculated matrix inversion.
 
-## Write a short comment describing this function
-
+## This function creates a matrix object with "methods" - 
+## set and get sets and gets the matix
+## setinverse and getinverse sets and gets the inversed matrix
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
 
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve function returns the inversion of matrix x (a matrix created with
+## makeCacheMatrix function).
+## If the result was previously calculated, 
+## then cached value should be returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
